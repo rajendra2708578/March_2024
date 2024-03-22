cbe71b23df3acda72a545e6b6b7bb4cb255caaf4
rajendra <nathrajen2710@gmail.com>
2024-03-22 23:56:37 +0530

    more added

---
+++ 21-03-2024/Diagonal sum in binary tree.cpp
@@ -0,0 +1,32 @@
+
+
+
+
+//this a functional code neither header files nor main function are included in  it.
+
+ vector <int> diagonalSum(Node* root) {
+        // Add your code here
+        vector<int>ans;
+        map<int,int>mp;
+        queue<pair<Node*,int>>pq;
+        pq.push({root,1});
+        while(!pq.empty())
+        {
+            auto it=pq.front();
+            pq.pop();
+            Node *no=it.first;
+            int val=it.second;
+            mp[val]+=it.first->data;
+            if(no->left)
+            pq.push({no->left,val-1});
+            if(no->right)
+            pq.push({no->right,val});
+        }
+        for(auto it:mp)
+        {
+            
+            ans.push_back(it.second);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
